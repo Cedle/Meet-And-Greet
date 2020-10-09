a27be2fe719a24266c7f644d29ce23428aae6ff9
Cedle <cedricjr46@gmail.com>
2020-10-09 14:56:58 +0200

    nex

--- public/index.js
+++ public/index.js
@@ -2,13 +2,13 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 49.784523, lng: 9.921944 },
     zoom: 8,
   });
 }
 function changemap(){
-  document.getElementById("map").style.visibility = 'visible';
   document.getElementById("home").style.visibility = 'hidden';
+  document.getElementById("map").style.visibility = 'visible';
   document.getElementById("neu").style.visibility = 'hidden';
   document.getElementById("chat").style.visibility = 'hidden';
   document.getElementById("profil").style.visibility = 'hidden';
@@ -19,5 +19,29 @@ function changehome(){
   document.getElementById("neu").style.visibility = 'hidden';
   document.getElementById("chat").style.visibility = 'hidden';
   document.getElementById("profil").style.visibility = 'hidden';
+
+}
+function changeneu(){
+  document.getElementById("home").style.visibility = 'hidden';
+  document.getElementById("map").style.visibility = 'hidden';
+  document.getElementById("neu").style.visibility = 'visible';
+  document.getElementById("chat").style.visibility = 'hidden';
+  document.getElementById("profil").style.visibility = 'hidden';
+
+}
+function changechat(){
+  document.getElementById("home").style.visibility = 'hidden';
+  document.getElementById("map").style.visibility = 'hidden';
+  document.getElementById("neu").style.visibility = 'hidden';
+  document.getElementById("chat").style.visibility = 'visible';
+  document.getElementById("profil").style.visibility = 'hidden';
+  
+}
+function changeprofil(){
+  document.getElementById("home").style.visibility = 'hidden';
+  document.getElementById("map").style.visibility = 'hidden';
+  document.getElementById("neu").style.visibility = 'hidden';
+  document.getElementById("chat").style.visibility = 'hidden';
+  document.getElementById("profil").style.visibility = 'visible';
 
 }
